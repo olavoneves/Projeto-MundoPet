b391931a8386e9e17effc9f9326d5ada436b5a42
Olavo Neves <olavo9neves@gmail.com>
2025-04-01 21:49:20 -0300

    adicionei hovers aos botões

---
+++ script.js
@@ -0,0 +1,11 @@
+const uploadBttn = document.getElementById('uploadButton')
+const uploadInput = document.getElementById('uploadInput')
+const botaoPublicar = document.getElementById('botao-publicar')
+
+uploadBttn.addEventListener('click', () => {
+    uploadInput.click();
+})
+
+botaoPublicar.addEventListener('click', () => {
+    
+})
